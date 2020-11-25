43fdd69bbb5e649b3733e19abf438a4fdb0ab61f
JohnDeVries <jdevries3133@gmail.com>
2020-11-24 22:45:56 -0500

    Added generic subgroup matching methods to teacherHelper.helper.Helper

--- teacherHelper/helper.py
+++ teacherHelper/helper.py
@@ -56,28 +56,129 @@ class Helper(OnCourseMixin, SillyMixin):
         if st := self.students.get(student_name.title()):
             logger.debug(f'Exact match for {st.name}')
             return st
+
+        # get nearest match
         closest_name, confidence = process.extractOne(
             student_name,
             self.students.keys()
         )
+
+        logger.debug(
+            f'{closest_name} is similar to {student_name} with a confidence of '
+            + str(confidence)
+        )
         if auto_yes:
-            res = True
+            match = True
         else:
-            res = self.match_in_terminal(student_name, closest_name)
-        if res and not auto_yes:
-            # allow match to pass regardless of threshold if match was provided
+            match = self.match_in_terminal(student_name, closest_name)
+        if match and not auto_yes:
+            # allow match to pass regardless of thmatchhold if match was provided
             # by user input
             return self.students[closest_name]
-        if res and auto_yes:
+        if match and auto_yes:
             if confidence > threshold:
                 logger.debug(
                     f'Fuzzy match; {student_name} == {closest_name}::'
                     f'confidence = {confidence}::threshold = {threshold}'
                 )
                 return self.students[closest_name]
+            logger.debug('Match rejected; confidence too low')
         if not auto_yes:  # provide feedback to user
             print('Student object not found. find_nearest_match will return None')
         # None will be returned if no return conditions are met
+
+    def exhaustive_search(self, name, subgroup=None, threshold=70):
+        """
+        Split the name into words and check if each word is a unique first or
+        last name within the subgroup. If calling this function frequently
+        on repeated datasets, try to cache the results because this function
+        is slow.
+        """
+        # try matching normally first with default threshold of 90
+        st = self.find_nearest_match(name, auto_yes=True)
+        if st:
+            logger.debug(f'Basic search for {name} returned {st.name}')
+            return st
+        logger.debug(f'Basic search for {name} returned None')
+        if (st := self.search_within_subgroup(name,
+                                              subgroup,
+                                              threshold=threshold)):
+            return st
+        logger.debug(f'Full name subgroup search for {name} returned None')
+
+        if not ' ' in name:
+            name_parts = [name]
+        else:
+            name_parts = ' '.split(name)
+
+        logger.debug(f'Searching through words {name_parts}')
+
+        # search each name part as each role
+        for np in name_parts:
+            for type_ in ['last_name', 'first_name']:
+                if (st := self.search_within_subgroup(np,
+                                                      subgroup,
+                                                      name_part=type_,
+                                                      threshold=threshold)):
+                    logger.debug(f'{name} matched with {st.name} by {type_}')
+                    return st
+            logger.debug(f'{type_} subgroup search for {name} returned None')
+
+    def search_within_subgroup(self, name, subgroup: list, threshold=90, name_part='name'):
+        """
+        subgroup should be a list of Student objects to search within. threshold
+        is the confidence threshold. name_part is the part of the name that
+        "name" is declared to be. Can be 'first_name', 'last_name', or just
+        'name' (full name). At a higher level, this allows more or less
+        exhaustive searching by calling this function multiple times.
+        """
+        logger.debug(f'Checking {name} as a {name_part}')
+        name_match = process.extract(
+            name,
+            [getattr(s, name_part) for s in subgroup],
+            limit=2
+        )
+
+        # Check whether name is unique within subgroup.
+        if name_match[1][0] == name_match[0][0]:
+            logger.debug(
+                f'Cannot proceed with {name}. More than one '
+                f'student in the subgroup has the first '
+                f'name {name_match[0][0]}.'
+            )
+
+        # check for confidence
+        if name_match[0][1] < threshold:
+            logger.debug(
+                f'{name_match[0][0]} rejected against {name} because the '
+                f'confidence of {name_match[0][1]} was below the threshold of '
+                + str(threshold)
+            )
+            return  # does not pass threshold
+
+        name = name_match[0][0]
+
+        if name == 'name':  # shortcut for full name matches
+            if (st := self.students[name]):
+                logger.debug('Hit on full name match shortcut')
+                return st
+
+        # we know the name is unique in the subgroup, so find the corresponding
+        # Student object in the subgroup and return it.
+        for st in self.students.values():
+
+            if not st in subgroup:
+                continue
+
+            # break when we find the right student
+            if name in st.name:
+                break
+
+        logger.debug(
+            f'SUBGROUP MATCH {name} matches with {st.name} within subgroup.'
+        )
+
+        return st
 
     def match_in_terminal(self, a, b):
         print('Do these names match? (y/n)')
