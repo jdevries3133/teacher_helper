83b5a96684a395dd0a1e9e12f278d412ec9eb403
JohnDeVries <jdevries3133@gmail.com>
2020-12-09 12:52:59 -0500

    added "emp email" shell utility

--- shell.py
+++ shell.py
@@ -16,14 +16,17 @@ from pathlib import Path
 
 from fuzzywuzzy import process
 
-from teacherHelper import Helper
+from teacherHelper import Helper, Email
 from paychex import Paychex
+
+# TODO: re-implement implement with argparse (https://docs.python.org/3/library/argparse.html)
 
 
 class ShellUtils:
 
     def __init__(self, args):
         self.args = args
+        self.helper = self.check_cache()  # will print help if cache doesn't exist
 
     def route(self):
         'Route self.args to various utilities'
@@ -36,11 +39,15 @@ class ShellUtils:
 
         # launch python shell if there are no arguments
         if len(self.args) < 2:
-            helper = self.check_cache()
-            code.interact(local={'helper': helper})
+            code.interact(local={
+                'helper': self.helper,
+                'Email': Email
+            })
             sys.exit()
 
         # route argument to utility functions
+
+        # COMMAND "emp student" => search for student info
         if self.args[1] == 'student':
             try:
                 st = self.student_search(
@@ -51,13 +58,17 @@ class ShellUtils:
                 sys.exit()
             except IndexError:
                 self.improper_usage()
+
+        # COMMAND "emp clock" => clock in to paychex (unstable)
         elif self.args[1] == 'clock':
             self.clock()
             sleep(1)
-        # silly timer
+
+        # COMMAND "emp timer" => start silly timer
         elif self.args[1] == 'timer':
             self.silly_timer()
-        # quick open google classrooms by tag name
+
+        # COMMAND "emp gc" => quick open google classrooms by tag name
         elif self.args[1] == 'gc':
             valid_tags = {
                 '6': 'https://classroom.google.com/u/0/c/MTU4NTE3OTg5MDc0',
@@ -72,7 +83,8 @@ class ShellUtils:
                     'Invalid google classroom tag name. Acceptable tags are:\n'
                     + '\t'.join(valid_tags.keys())
                 )
-        # search for student by parent
+
+        # COMMAND "emp parent" => search for student by parent
         elif self.args[1] == 'parent':
             try:
                 match = self.search_by_parent(
@@ -82,6 +94,9 @@ class ShellUtils:
                     print(match)
             except IndexError:
                 self.improper_usage()
+
+        # COMMAND "emp report" => grep for zoom attendance history through
+                                  # downloaded reports
         elif self.args[1] == 'report':
             try:
                 st = self.check_cache().find_nearest_match(
@@ -105,16 +120,87 @@ class ShellUtils:
                 )
             except IndexError:
                 self.improper_usage()
+        elif self.args[1] == 'email':
+            self.email()
         else:
             self.improper_usage()
 
+    def email(self):
+        """
+        THIS ONLY WORKS FOR EMAILING STUDENTS. It uses the lookup function
+        to grab their email, which is where the true convenience lies; it
+        saves the step of searching for the student email since their emails
+        are gibberish.
+
+        TODO: implement CC'ing
+        """
+
+        # assemble student name and message
+        data = {'name': '', 'message': '', 'subject': ''}
+        appending_to = 'name'
+        for word in self.args[1:]:
+            if not word:
+                continue
+            if word == '--message':
+                appending_to = 'message'
+                continue
+            if word == '--subject':
+                appending_to = 'subject'
+                continue
+            data[appending_to] += word + ' '
+        data = {k: v.strip() for k, v in data.items()}
+
+        # confirm details
+        if not (st := self.confirm_email_data(data)):
+            print('Ok, email not sent')
+            sys.exit()
+
+        # send email
+        with Email() as emlr:
+            emlr.send(
+                to=st.email,
+                subject=data['subject'],
+                message=data['message'].split(' ')
+            )
+
+    def confirm_email_data(self, data):
+        """
+        Returns confirmed st (Student object instance) or False
+        """
+        st = self.helper.find_nearest_match(data.get('name'), auto_yes=True)
+        if self._confirm(
+            f"Is this who you want to email? {st.name} (y/n): "
+        ) and self._confirm(
+            f'Is this the correct subject?\n\t\"{data.get("subject")}\"\n(y/n) '
+        ) and self._confirm(
+            f'Is this the correct message?\n\t\"{data.get("message")}\"\n(y/n) '
+        ):
+            return st
+        return False
+
+    @ staticmethod
+    def _confirm(message):
+        inp = None
+        while not (inp := inp if inp else None):
+            inp = input(message)
+            if inp.lower() == 'y':
+                return True
+            if inp.lower() == 'n':
+                return False
+            print('Please enter "y" or "n"')
+
+
+
+
+
+
     def search_by_parent(self, name):
-        helper = self.check_cache()
+        self.helper
 
         # create dict of guardians & primary_contacts
         all_guardians = {}
         primary_contacts = {}
-        for st in helper.students.values():
+        for st in self.helper.students.values():
             try:
                 primary = st.primary_contact
                 primary_contacts.setdefault(primary.name, primary)
