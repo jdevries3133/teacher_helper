187a6e8885762bae510fd88c64db41a28e6b4bc8
Jack DeVries <jdevries3133@gmail.com>
2021-03-05 00:00:00 +0000

    feat: emailer accepts ready-to-send html

--- teacherHelper/email_.py
+++ teacherHelper/email_.py
@@ -1,3 +1,4 @@
+from typing import Union, List, NamedTuple
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from pathlib import Path
@@ -6,8 +7,13 @@ import ssl
 import os
 import logging
 
+from bs4 import BeautifulSoup
+
 logger = logging.getLogger(__name__)
 
+class Message(NamedTuple):
+    html: str
+    plain_text: str
 
 class Email:
     def __init__(self, username=None, password=None):
@@ -38,29 +44,17 @@ class Email:
     def __exit__(self, *_):
         self.connection.close()
 
-    def send(self, *, to, subject, message: list, html=False):
+    def send(self, *, to, subject, message: Union[Message, List[str]], cc=None, html=False):
         """
-        Accepts kwargs ONLY
+        If message is a list, its strings will be wrapped in <p> tags before
+        being inserted into the email.
 
-        Send plain text emails en masse.
-
-        Set html, and the message should be a tuple:
-            message[0] = plain text
-            message[1] = html
-
-        The html will still be inserted into the default template, including
-        your signature.
+        If message is a tuple, it will be presumed to be 
         """
-        if not isinstance(message, list):
-            raise Exception(
-                'Message must be a list; an array of paragraphs. Each string '
-                'in the list will be mapped to a <p> tag in the html, or '
-                'joined with a double newline in the plain text.'
-            )
         if not self.connection:
             raise Exception(
                 'Connection must be established in __enter__. Use this class '
-                'with a context manager.'
+                'as a context manager.'
             )
         # init ssl
         me = 'jdevries@empacad.org'
@@ -68,34 +62,59 @@ class Email:
         msg['Subject'] = subject
         msg['From'] = me
         msg['To'] = to
-        html, text = self.make_message(message)
-        part1 = MIMEText(text, 'plain')
+        if cc:
+            msg['Cc'] = cc
+        if isinstance(message, list):
+            html, plain_text = self.make_message(message)
+        elif isinstance(message, Message):
+            html, plain_text = message
+        else:
+            raise TypeError(
+                f'Invalid message type: {type(message)}. Message must be of '
+                'type list or teacherHelper.email.Message'
+            )
+        part1 = MIMEText(plain_text, 'plain')
         part2 = MIMEText(html, 'html')
         msg.attach(part1)
         msg.attach(part2)
         self.connection.sendmail(me, to, msg.as_string())
 
-    def make_message(self, message: list, raise_exception=True):
+    def make_message(self, message: Union[list, str], *, template: Path=None):
         """
-        Make a message with the default html template.
+        Make a message with an html tempalte. A list will be interpreted
+        as a list of paragraphs to be inserted while a string is expected
+        to be html text ready to be fully inserted.
         """
+        if not template:
+            template = Path(self.template_dir, 'default.html')
         # load template
-        with open(Path(self.template_dir, 'default.html'), 'r') as htmlf:
-            html_message = htmlf.read()
+        with open(template, 'r') as htmlf:
+            html_template = htmlf.read()
 
-        # generate html; text strings become paragraphs and blank strings
-        # become <br /> elements.
-        html = []
-        for i in message:
-            if i:
-                html.append(
-                    "<p style=\"color: black; font-family: Helvetica, Arial, Sans-Serif;\">"
-                      f"{i}"
-                    "</p>"
-                )
-            else:
-                html.append('<br />')
+        if isinstance(message, list):
+            # generate html paragraphs from list of strings
+            html = []
+            for i in message:
+                if i:
+                    html.append(
+                        "<p style=\"color: black; font-family: Helvetica, Arial, Sans-Serif;\">"
+                          f"{i}"
+                        "</p>"
+                    )
+                else:
+                    html.append('<br />')
+            html = '\n'.join(html)
+            message = '\n\n'.join(message)
+        elif isinstance(message, str):
+            # presume string should be directly inserted; already valid html
+            html = message
+            message = '\n'.join(BeautifulSoup(message, features='lxml').findAll(text=True))
+        else:
+            raise TypeError(
+                f'Invalid message type: {type(message)}. Message must be of '
+                'type list or str'
+            )
         return (
-            html_message.replace('{{insert}}', '\n'.join(html)),
-            '\n\n'.join(message)
+            html_template.replace('{{ email_content }}', html),
+            message
         )
