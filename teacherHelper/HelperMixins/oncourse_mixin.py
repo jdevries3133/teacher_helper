200444883ab6ec1be95ed0c5b36f6e3d7d10134b
Jack DeVries <jdevries3133@gmail.com>
2021-08-03 11:05:07 -0400

    feat: IterCsv can strip row values

--- teacherHelper/HelperMixins/oncourse_mixin.py
+++ teacherHelper/HelperMixins/oncourse_mixin.py
@@ -28,56 +28,54 @@ class OnCourseMixin:
 
         with open(student_data, 'r', encoding='utf-8-sig') as csvfile:
             rows = [r for r in csv.reader(csvfile, delimiter=',')]
-            # TODO: load birthday field as datetime
-            # TODO: make thsi less repetitive. Just use a named collection.
-            acceptable_headers = [
-                'first name',
-                'last name',
-                'grade level',
-                'homeroom teacher',
-                'email address',
-                'birth date',
-            ]
-            for context, row in IterCsv(acceptable_headers, rows, strict=strict_headers):
-                (
-                    first,
-                    last,
-                    grade,
+        acceptable_headers = [
+            'first name',
+            'last name',
+            'grade level',
+            'homeroom teacher',
+            'email address',
+            'birth date',
+        ]
+        for get_item in IterCsv(acceptable_headers, rows, strict=strict_headers):
+            (
+                first,
+                last,
+                grade,
+                homeroom,
+                email,
+                birthday,
+            ) = (
+                get_item('first name'),
+                get_item('last name'),
+                get_item('grade level'),
+                get_item('homeroom teacher'),
+                get_item('email address'),
+                get_item('birth date'),
+            )
+            # convert grade to int
+            for i in [4, 5, 6]:
+                if grade.find(str(i)) != -1:
+                    grade = i
+                    break
+            student = Student(
+                {
+                    'first_name': first,
+                    'last_name': last,
+                    'grade_level': grade,
+                    'homeroom': homeroom,
+                    'email': email,
+                    'birthday': birthday,
+                }
+            )
+            STUDENTS[first + ' ' + last] = student
+            if homeroom not in HOMEROOMS:
+                HOMEROOMS[homeroom] = Homeroom(
                     homeroom,
-                    email,
-                    birthday,
-                ) = (
-                    row[context['first name']],
-                    row[context['last name']],
-                    row[context['grade level']],
-                    row[context['homeroom teacher']],
-                    row[context['email address']],
-                    row[context['birth date']],
+                    grade,
+                    [student],
                 )
-                # convert grade to int
-                for i in [4, 5, 6]:
-                    if grade.find(str(i)) != -1:
-                        grade = i
-                        break
-                student = Student(
-                    {
-                        'first_name': first,
-                        'last_name': last,
-                        'grade_level': grade,
-                        'homeroom': homeroom,
-                        'email': email,
-                        'birthday': birthday,
-                    }
-                )
-                STUDENTS[first + ' ' + last] = student
-                if homeroom not in HOMEROOMS:
-                    HOMEROOMS[homeroom] = Homeroom(
-                        homeroom,
-                        grade,
-                        [student],
-                    )
-                else:
-                    HOMEROOMS[homeroom].students.append(student)
+            else:
+                HOMEROOMS[homeroom].students.append(student)
 
         # instantiation
         self = cls(
@@ -85,97 +83,92 @@ class OnCourseMixin:
         )
         with open(guardian_data, 'r', encoding='utf8') as csvfile:
             rows = [r for r in csv.reader(csvfile)]
-            acceptable_headers = [
-                'guardian first name',
-                'guardian last name',
-                'student first name',
-                'student last name',
-                'primary contact',
-                'guardian email address',
-                'guardian mobile phone',
-                'guardian phone',
-                'guardian work phone',
-                'comments',
-                'allow contact',
-                'student resides with',
-                'relation to student'
-            ]
-            for context, row in IterCsv(acceptable_headers, rows, strict=strict_headers):
-                # create raw context dict of all strings
-                if not context.get('student resides with'):
-                    raise Exception(
-                        'Remember, "student resides with"  is misspelled in '
-                        'OnCourse. Fix it in the CSV you downloaded.'
-                    )
-                # convert spreadsheet rows to attributes of student class.
-                raw_context = {
-                    'first_name': row[context.get('guardian first name')],
-                    'last_name': row[context.get('guardian last name')],
-                    'student': row[context.get('student first name')] + ' ' + row[context.get('student last name')],
-                    'primary_contact': row[context.get('primary contact')],
-                    'email': row[context.get('guardian email address')],
-                    'mobile_phone': row[context.get('guardian mobile phone')],
-                    'home_phone': row[context.get('guardian phone')],
-                    'work_phone': row[context.get('guardian work phone')],
-                    'comments': row[context.get('comments')],
-                    'allow_contact': row[context.get('allow contact')],
-                    'student_resides_with': row[context.get('student resides with')],
-                    'relationship_to_student': row[context.get('relation to student')],
-                }
-                clean_context = {}
-                # find student object match
-                student = self.find_nearest_match(
-                    raw_context['student'],
-                    auto_yes=True
+        acceptable_headers = [
+            'guardian first name',
+            'guardian last name',
+            'student first name',
+            'student last name',
+            'primary contact',
+            'guardian email address',
+            'guardian mobile phone',
+            'guardian phone',
+            'guardian work phone',
+            'comments',
+            'allow contact',
+            'student resides with',
+            'relation to student'
+        ]
+        for get_item in IterCsv(acceptable_headers, rows, strict=strict_headers):
+            raw_context = {
+                'first_name': get_item('guardian first name'),
+                'last_name': get_item('guardian last name'),
+                'student': get_item('student first name')
+                            + ' '
+                            + get_item('student last name'),
+                'primary_contact': get_item('primary contact'),
+                'email': get_item('guardian email address'),
+                'mobile_phone': get_item('guardian mobile phone'),
+                'home_phone': get_item('guardian phone'),
+                'work_phone': get_item('guardian work phone'),
+                'comments': get_item('comments'),
+                'allow_contact': get_item('allow contact'),
+                'student_resides_with': get_item('student resides with'),
+                'relationship_to_student': get_item('relation to student'),
+            }
+            clean_context = {}
+            # find student object match
+            student = self.find_nearest_match(
+                raw_context['student'],
+                auto_yes=True
+            )
+            if not student:
+                continue
+            if student.name != raw_context['student']:
+                raise Exception(
+                    f"Integrity error. {student.name} does not equal "
+                    + raw_context['student']
                 )
-                if not student:
-                    continue
-                if student.name != raw_context['student']:
-                    raise Exception(
-                        f"Integrity error. {student.name} does not equal "
-                        + raw_context['student']
-                    )
-                clean_context['student'] = student
-                for k, v in raw_context.items():
-                    # clean phone numbers
-                    if 'phone' in k:
-                        if '_' in v:
-                            continue
-                        nums = [l for l in v if l.isnumeric()]
-                        if not nums:
-                            continue
-                        if len(nums) < 10:
-                            continue
-                        if len(nums) > 11:
-                            raw_context['comments'] += f'\n{k} is {v}'
-                            continue
-                        try:
-                            phone_number = int(''.join(nums))
-                            clean_context[k] = phone_number
-                        except TypeError:
-                            continue
-                    # convert boolean fields to boolean
-                    if k in [
-                        'primary_contact',
-                        'allow_contact',
-                        'student_resides_with',
-                    ]:
-                        if 'Y' in v:
-                            v = True
-                        elif 'N' in v:
-                            v = False
-                        else:
-                            raise OnCourseBooleanConversionError(
-                                f'Supposedly boolean field {k} could not'
-                                'be converted into a boolean value.'
-                            )
-                        clean_context[k] = v
-                    clean_context.setdefault(k, v)
-                parent = ParentGuardian(clean_context)
-                student.guardians.append(parent)
-                if clean_context['primary_contact']:
-                    # It is important that the primary_contact attribute of the
-                    # student is assigned while parent / guardian data is being
-                    # parsed.
-                    student.primary_contact = parent
+            clean_context['student'] = student
+            for k, v in raw_context.items():
+                # clean phone numbers
+                if 'phone' in k:
+                    if '_' in v:
+                        continue
+                    nums = [l for l in v if l.isnumeric()]
+                    if not nums:
+                        continue
+                    if len(nums) < 10:
+                        continue
+                    if len(nums) > 11:
+                        raw_context['comments'] += f'\n{k} is {v}'
+                        continue
+                    try:
+                        phone_number = int(''.join(nums))
+                        clean_context[k] = phone_number
+                    except TypeError:
+                        continue
+                # convert boolean fields to boolean
+                if k in [
+                    'primary_contact',
+                    'allow_contact',
+                    'student_resides_with',
+                ]:
+                    if 'Y' in v:
+                        v = True
+                    elif 'N' in v:
+                        v = False
+                    else:
+                        raise OnCourseBooleanConversionError(
+                            f'Supposedly boolean field {k} could not'
+                            'be converted into a boolean value.'
+                        )
+                    clean_context[k] = v
+                clean_context.setdefault(k, v)
+            parent = ParentGuardian(clean_context)
+            student.guardians.append(parent)
+            if clean_context['primary_contact']:
+                # It is important that the primary_contact attribute of the
+                # student is assigned while parent / guardian data is being
+                # parsed.
+                student.primary_contact = parent
         return self
--- teacherHelper/tools/csv_parser.py
+++ teacherHelper/tools/csv_parser.py
@@ -1,15 +1,8 @@
-from abc import ABC
-from pathlib import Path
-
-
-class BaseCsvParser(ABC):
-    def __init__(self, csv_dir: Path):
-        assert isinstance(csv_dir, Path)
-        self.csv_dir = csv_dir
+class IterCsvException(Exception): ...
 
 
 class IterCsv:
-    def __init__(self, acceptable_headers, rows, strict=True):
+    def __init__(self, acceptable_headers, rows, strict=True, strip_data=True):
         """
         Generator returns context, and row. Context is a dict in which
         the key is one of the acceptable headers, and the value is the
@@ -18,11 +11,18 @@ class IterCsv:
         self.current_row = 0
         self.rows = rows
         self.context = {}
-        # assign context
-        for i, raw_header in enumerate(rows[0]):
+        self._acceptable_headers = acceptable_headers
+        self._strict = strict
+        self._strip_data = strip_data
+        self._assign_context()
+
+    def _assign_context(self):
+        """Context is a mapping of header labels to row indices, so that the
+        row items can later be fetched by name."""
+        for i, raw_header in enumerate(self.rows[0]):
             raw_header = raw_header.lower()
-            for clean_header in acceptable_headers:
-                if not strict and clean_header in raw_header or raw_header in clean_header:
+            for clean_header in self._acceptable_headers:
+                if not self._strict and clean_header in raw_header or raw_header in clean_header:
                     before = len(self.context)
                     self.context[clean_header] = i
                     after = len(self.context)
@@ -32,7 +32,7 @@ class IterCsv:
                             + clean_header
                             + '\nIf raw header matching is to lenient, set strict=True'
                         )
-                elif strict and clean_header == raw_header:
+                elif self._strict and clean_header == raw_header:
                     before = len(self.context)
                     self.context[clean_header] = i
                     after = len(self.context)
@@ -42,6 +42,16 @@ class IterCsv:
                             f'{raw_header}. Edit the csv to differentiate'
                             'between these columns to continue.'
                         )
+        self._validate_context(self.context)
+
+    def fetch(self, name: str):
+        """Fetch an item from a row by name during iteration."""
+        if (index := self.context.get(name)) is None:
+            raise IterCsvException(f'{name} does not exist in csv context')
+        value = self.rows[self.current_row][index]
+        if self._strip_data:
+            return value.strip()
+        return value
 
     def __iter__(self):
         return self
@@ -49,5 +59,17 @@ class IterCsv:
     def __next__(self):
         self.current_row += 1
         if self.current_row < len(self.rows) - 1:
-            return self.context, self.rows[self.current_row]
+            return self.fetch
         raise StopIteration
+
+    @ staticmethod
+    def _validate_context(context):
+        """Because this field is misspelled in OnCourse....."""
+        is_parent_spreadsheet = 'guardian first name' in context
+        is_header_misspelled = context.get('student resides with') is None
+
+        if is_parent_spreadsheet and is_header_misspelled:
+            raise IterCsvException(
+                'Remember, "student resides with"  is misspelled in '
+                'OnCourse. Fix it in the CSV you downloaded.'
+            )
